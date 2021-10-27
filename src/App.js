1fb769b777dedca768aea32b8fb9ac81b97d5810
Luiz088 <luiz.silva326@fatec.sp.gov.br>
2021-10-27 01:28:58 +0000

    (26/10) Rodapé, inicio da estrutura de tabela

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AppHeader from './ui/AppHeader'
 import { createTheme, ThemeProvider } from '@mui/material';
 import { yellow, pink } from '@mui/material/colors';
+import Box from '@mui/material/Box';
 
 import ClientesList from './routed/ClientesList'
 import ClientesForm from './routed/ClientesForm'
+import AppFooter from './ui/AppFooter';
 
 const customTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: 'light',
     primary: {
       main: yellow[500]
     },
@@ -23,17 +25,21 @@ function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={customTheme}>
-        <AppHeader bgcolor={customTheme.palette.primary.main} />
-        <Switch>
+      <Box sx={{ height: '100vh', backgroundColor: customTheme.palette.background.default, color: customTheme.palette.text.primary }}>
+          <AppHeader bgcolor={customTheme.palette.primary.main} />
+          <Switch>
+            <Box component="main" sx={{ margin: '20px 20px 60px 20px'}}>
+              <Route path="/clientes" exact>
+                <ClientesList />
+              </Route>
 
-          <Route path="/clientes" exact>
-            <ClientesList />
-          </Route>
-
-          <Route path="/clientes/new" exact>
-            <ClientesForm />
-          </Route>
-        </Switch>
+              <Route path="/clientes/new" exact>
+                <ClientesForm />
+              </Route>
+            </Box>
+          </Switch>
+          <AppFooter />
+        </ Box>
       </ThemeProvider>
     </BrowserRouter>
   );
